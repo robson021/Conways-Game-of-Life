9fb7428ff0afda7e04cefc2a0ed71632bc52d40d
robert <nowak021@gmail.com>
2016-04-23 12:51:20 +0200

    a

--- src/DrawingPanel.java
+++ src/DrawingPanel.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
 import java.awt.*;
 
 /**
@@ -6,22 +7,27 @@ import java.awt.*;
  */
 public class DrawingPanel extends JPanel {
     private static final int SIZE = 20;
-    //public static final int WINDOW_SIZE = SIZE * 32;
+    private static final String EMPTY_TEXT = "    ", ALIVE = "X";
     private boolean[][] cells = new boolean[SIZE][SIZE];
     private JLabel[][] fields = new JLabel[SIZE][SIZE];
 
     public DrawingPanel() {
-        super(new GridLayout(SIZE, SIZE));
+        super(new GridLayout(SIZE, SIZE, 3, 3));
         clearCells();
-        init();
+        drawTiles();
 
         //this.setSize(new Dimension(WINDOW_SIZE, WINDOW_SIZE));
     }
 
-    private void init() {
+    private void drawTiles() {
         for (int j, i = 0; i < SIZE; i++)
             for (j = 0; j < SIZE; j++) {
-                fields[i][j] = new JLabel("x");
+                JLabel l;
+                if (cells[i][j] == false) l = new JLabel(EMPTY_TEXT, JLabel.CENTER);
+                else l = new JLabel(ALIVE, JLabel.CENTER);
+                l.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
+                l.setFont(l.getFont().deriveFont(20f));
+                fields[i][j] = l;
                 this.add(fields[i][j]);
             }
 
@@ -38,26 +44,15 @@ public class DrawingPanel extends JPanel {
     public boolean addNewLife(int x, int y) {
         if (x < SIZE && y < SIZE) {
             cells[x][y] = true;
-        } else {
-            return false;
+            repaint();
+            return true;
         }
-        repaint();
-        return true;
+        return false;
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        for (int j, i = 0; i < SIZE; i++) {
-            for (j = 0; j < SIZE; j++) {
-                if (cells[i][j] == true) {
-                    fields[i][j] = new JLabel("o");
-                } else {
-                    fields[i][j] = new JLabel("x");
-                }
-
-            }
-        }
-        repaint();
+        this.drawTiles();
     }
 }
